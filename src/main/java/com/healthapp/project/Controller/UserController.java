f8c0d732e7e56e0f23ac29a688b0ad40dba68427
Dex Joseph <dex@mac-mini.mynetworksettings.com>
2023-02-27 18:21:31 -0500

    added stuff

--- src/main/java/com/healthapp/project/Controller/UserController.java
+++ src/main/java/com/healthapp/project/Controller/UserController.java
@@ -2,23 +2,19 @@ package com.healthapp.project.Controller;
 
 import com.healthapp.project.Entity.User;
 import com.healthapp.project.Service.UserService;
+import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-
-import java.util.Collection;
-
 @RestController
 @RequestMapping("/")
 public class UserController {
-@Autowired
-    private UserService userService;
+  @Autowired private UserService userService;
 
-@RequestMapping(method = RequestMethod.GET)
-    public Collection<User> getAllUsers(){
-        return userService.getAllUsers();
-    }
-
+  @RequestMapping(method = RequestMethod.GET)
+  public Collection<User> getAllUsers() {
+    return userService.getAllUsers();
+  }
 }
--- src/main/java/com/healthapp/project/Entity/User.java
+++ src/main/java/com/healthapp/project/Entity/User.java
@@ -1,29 +1,29 @@
 package com.healthapp.project.Entity;
 
 public class User {
-    private final String firstName;
-    private final String lastName;
-    private int idNumber;
+  private final String firstName;
+  private final String lastName;
+  private int idNumber;
 
-    public User(String firstName,String lastName, int idNumber){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.idNumber = idNumber;
-    }
+  public User(String firstName, String lastName, int idNumber) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.idNumber = idNumber;
+  }
 
-    public String getFirstName() {
-        return firstName;
-    }
+  public String getFirstName() {
+    return firstName;
+  }
 
-    public String getLastName() {
-        return lastName;
-    }
+  public String getLastName() {
+    return lastName;
+  }
 
-    public int getIdNumber() {
-        return idNumber;
-    }
+  public int getIdNumber() {
+    return idNumber;
+  }
 
-    public void setIdNumber(int idNumber) {
-        this.idNumber = idNumber;
-    }
+  public void setIdNumber(int idNumber) {
+    this.idNumber = idNumber;
+  }
 }
