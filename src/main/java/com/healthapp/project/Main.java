c3509585e477b4614741e4fee1165a7c1ca1bfc6
Dex Joseph <dex@mac-mini.mynetworksettings.com>
2023-03-06 15:38:08 -0500

    still working on postgresql connection

--- src/main/java/com/healthapp/project/Main.java
+++ src/main/java/com/healthapp/project/Main.java
@@ -1,12 +1,31 @@
 package com.healthapp.project;
 
+import com.healthapp.project.DAO.DataSourceConfig;
+import com.healthapp.project.Service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Main {
 
+
   public static void main(String[] args) {
-    SpringApplication.run(Main.class, args);
+   SpringApplication.run(Main.class, args);
+
+   ApplicationContext context = new AnnotationConfigApplicationContext(DataSourceConfig.class);
+      DataSource dataSource = context.getBean(DataSource.class);
+     UserService userService = new UserService(dataSource);
+     System.out.print(userService.isDBConnected());
+
+    System.out.println(System.getenv("DB_URL_TEST"));
+    System.out.println(System.getenv("DB_USERNAME_TEST"));
+    System.out.println(System.getenv("DB_PASSWORD_TEST"));
+
+
   }
 }
